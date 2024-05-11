9d9d4668d063eba6db7f203f73fa976d2bf4b3f5
MariskaEsaPurnomo <mariskaesapurnomo@gmail.com>
2024-05-11 11:56:48 +0700

    prosedur peek dengan conditional statement jika top == NULL

--- stuckusinglinkedlist/stuckusinglinkedlist.cpp
+++ stuckusinglinkedlist/stuckusinglinkedlist.cpp
@@ -44,6 +44,14 @@ public:
 		top = top->next;	// update the top pointer to the next node
 	}
 
+	// peek/top operation: retrieve the value of the topmost element without removing it
+	void peek() {
+		if (top == NULL) {
+			cout << "List is empty." << endl;
+		}
+
+	}
+
 };
 
 int main()
