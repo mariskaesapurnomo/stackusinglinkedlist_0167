36a18da3d91cb0e8f0072dc9a2829073393404d2
MariskaEsaPurnomo <mariskaesapurnomo@gmail.com>
2024-05-11 11:28:18 +0700

    membuat modifier public pada class stack

--- stuckusinglinkedlist/stuckusinglinkedlist.cpp
+++ stuckusinglinkedlist/stuckusinglinkedlist.cpp
@@ -20,6 +20,10 @@ class Stack {
 private:
 	Node* top;		//pointer to the top node of the stack
 
+public:
+	Stack() {
+		top = NULL;		// initialize the stack with a null top pointer
+	}
 };
 
 int main()
