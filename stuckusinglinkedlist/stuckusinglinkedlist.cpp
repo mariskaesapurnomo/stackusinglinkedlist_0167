27850fb3bec006724499f5da00221ed24d78c581
MariskaEsaPurnomo <mariskaesapurnomo@gmail.com>
2024-05-11 11:40:40 +0700

    membuat fungsi push value

--- stuckusinglinkedlist/stuckusinglinkedlist.cpp
+++ stuckusinglinkedlist/stuckusinglinkedlist.cpp
@@ -24,6 +24,17 @@ public:
 	Stack() {
 		top = NULL;		// initialize the stack with a null top pointer
 	}
+
+	//oush operation: insert an element onto the top of the stack
+	int push(int value) {
+		Node* newNode = new Node();		//1. Allocate memory of the new node
+		newNode->data = value;			// 2. assign value
+		newNode->next = top;			// 3. set the next pointer of the new node to the current top node
+		top = newNode;					//4. update the top pointer to the new node
+		cout << "Push value: " << value << endl;
+		return value;
+	}
+
 };
 
 int main()
