496c735a39a21ab098455a486c26fa57a46f2a9d
MariskaEsaPurnomo <mariskaesapurnomo@gmail.com>
2024-05-11 11:15:07 +0700

    Add project files.

---
+++ stuckusinglinkedlist/stuckusinglinkedlist.cpp
@@ -0,0 +1,15 @@
+// stuckusinglinkedlist.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+//node class representing a single node in the linked list
+class Node {
+
+};
+
+int main()
+{
+  
+}
